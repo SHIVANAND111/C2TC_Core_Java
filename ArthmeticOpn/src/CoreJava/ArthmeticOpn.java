877652d97ee0639271a641bd3db56330be9f6623
SHIVANAND <shivanand12546@gmail.com>
2022-05-29 23:05:07 +0530

    Arthmatic oparations

---
+++ ArthmeticOpn/src/CoreJava/ArthmeticOpn.java
@@ -0,0 +1,14 @@
+package CoreJava;
+
+public class ArthmeticOpn {
+	public static void main(String []args)
+	{
+
+	int a=10;
+	int b=4;
+int c=15;
+int d=++a+b+c+--b+c+++--a+b++;
+System.out.println(d);
+	}
+
+}
