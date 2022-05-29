7201b1b15e859c3a32284cf63532180f7e1852dc
SHIVANAND <shivanand12546@gmail.com>
2022-05-29 23:39:58 +0530

    Instance variables

---
+++ InstanceVariable/src/CoreJava/InstanceVariable.java
@@ -0,0 +1,15 @@
+package CoreJava;
+
+public class InstanceVariable {
+	int a=10;
+	int b=15;
+	int c= a+b;
+	public static void main (String[]args)
+	{
+	InstanceVariable obj=new InstanceVariable();
+
+	System.out.println(obj.c);
+	}
+	}
+
+
