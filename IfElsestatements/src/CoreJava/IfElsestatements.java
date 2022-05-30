dbac8d28d07a5c2a6651b0a21a6d6773fc8264b0
SHIVANAND <shivanand12546@gmail.com>
2022-05-30 18:17:31 +0530

    If else statements

---
+++ IfElsestatements/src/CoreJava/IfElsestatements.java
@@ -0,0 +1,20 @@
+package CoreJava;
+
+public class IfElsestatements {
+	public static void main(String[]args)
+	{
+		int a=20;
+		int b=25;
+	
+		if(a>b)
+		{
+			System.out.println("a is greater than b");
+			System.out.println("yes a greater than b");
+		}
+		else
+			System.out.println("b is greater than a");
+		System.out.println(" welecome to java");
+		
+	}
+
+}
