089ea76ce009bd5ac842feab7afe84910f68a516
SHIVANAND <shivanand12546@gmail.com>
2022-05-30 17:56:19 +0530

    If statements

---
+++ IfStatements/src/CoreJava/IfStatements.java
@@ -0,0 +1,19 @@
+package CoreJava;
+
+public class IfStatements {
+	public static void Getonthebus()
+	{
+		System.out.println("buss number is correct Getonthebus");
+	}
+	public static void main(String[]args)
+	{
+		int busnumber=1000;
+		
+		if(busnumber>600)
+			IfStatements.Getonthebus();
+		System.out.println("namm BMTC bangalore");
+	
+	}
+
+}
+
